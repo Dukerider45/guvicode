cb7d8e3c07eaf16f5d2bc6d83afc07c7d2792a85
Ramanathan12498 <34570185+Ramanathan12498@users.noreply.github.com>
2018-06-12 23:29:16 +0530

    Update larger.py

--- larger.py
+++ larger.py
@@ -1,9 +1,33 @@
 a=int(raw_input())
+
 b=int(raw_input())
+
 c=int(raw_input())
-if(a>b and a>c):
-	print(a)
-elif(b>a and b>c):	
-	print(b)
+if(a!=b and a!=c and b!=c):
+
+	if(a>b and a>c):
+	
+		print(a)
+
+	elif(b>a and b>c):		
+
+		print(b)
+
+	else:
+	
+		print(c)
+elif (a==b):
+	if(b>c):
+		print(b)
+	else:
+		print(c)
+elif (a==c):
+	if(b>c):
+		print(b)
+	else:
+		print(c)
 else:
-	print(c)
+	if(a>b):
+		print(a)
+	else:
+		print(b)
