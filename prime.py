1a585c92bae6084e123ba3e7d2464f8ec539d668
Ramanathan12498 <34570185+Ramanathan12498@users.noreply.github.com>
2018-06-14 11:57:43 +0530

    Update prime.py

--- prime.py
+++ prime.py
@@ -2,8 +2,8 @@ a=int(raw_input())
 def prime(x):
 	for i in range(2,x):
 		if(x%i<>0):
-			return("prime")
+			return("yes")
 		else:
-			return("not a prime")
+			return("no")
 c=prime(a)
 print(c)
